adfc2d3ae54a5774f1447e97a68540f9733b7208
nadine-shi <nadinshi@gmail.com>
2022-04-10 21:47:35 +0300

    Change error handling in WEB API

--- vue-mobile/files-web-api.js
+++ vue-mobile/files-web-api.js
@@ -16,10 +16,9 @@ export default {
         }
         return []
       })
-      .catch(() => {
-        return []
-      })
+      .catch(error => [])
   },
+
   getStorages: async () => {
     return webApi.sendRequest({
       moduleName: 'Files',
@@ -32,42 +31,29 @@ export default {
         }
         return []
       })
-      .catch(() => {
-        return []
-      })
+      .catch(error => [])
   },
+
   renameItem: async (parameters) => {
     return webApi.sendRequest({
       moduleName: 'Files',
       methodName: 'Rename',
       parameters: parameters,
     })
-      .then((result) => {
-        if (result) {
-          return result
-        }
-        return false
-      })
-      .catch(() => {
-        return false
-      })
+      .then(result => result)
+      .catch(error => false)
   },
+
   deleteItems: async (parameters) => {
     return webApi.sendRequest({
       moduleName: 'Files',
       methodName: 'Delete',
       parameters: parameters,
     })
-      .then((result) => {
-        if (result) {
-          return result
-        }
-        return false
-      })
-      .catch(() => {
-        return false
-      })
+      .then(result => result)
+      .catch(error => false)
   },
+
   copyMoveItems: async (parameters, method) => {
     return webApi.sendRequest({
       moduleName: 'Files',
@@ -75,16 +61,10 @@ export default {
       parameters: parameters,
       defaultErrorText: i18n.global.tc('FILESWEBCLIENT.ERROR_FILES_MOVE_PLURAL', parameters.Files.length ),
     })
-      .then((result) => {
-        if (result) {
-          return result
-        }
-        return false
-      })
-      .catch(() => {
-        return false
-      })
+      .then(result => result)
+      .catch(error => false)
   },
+
   createFolder: async (parameters) => {
     return webApi.sendRequest({
       moduleName: 'Files',
@@ -92,16 +72,10 @@ export default {
       parameters: parameters,
       defaultErrorText: i18n.global.t('FILESWEBCLIENT.ERROR_INVALID_FOLDER_NAME'),
     })
-      .then((result) => {
-        if (result) {
-          return result
-        }
-        return false
-      })
-      .catch(() => {
-        return false
-      })
+      .then(result => result)
+      .catch(error => false)
   },
+
   createShareableLink: async (parameters, module) => {
     return webApi.sendRequest({
       moduleName: module,
@@ -113,74 +87,49 @@ export default {
         if (result) return result
         return false
       })
-      .catch(() => {
-        return false
-      })
+      .catch(error => false)
   },
+
   deletePublicLink: async (parameters) => {
     return webApi.sendRequest({
       moduleName: 'Files',
       methodName: 'DeletePublicLink',
       parameters: parameters,
     })
-      .then((result) => {
-        if (result) {
-          return result
-        }
-        return false
-      })
-      .catch(() => {
-        return false
-      })
+      .then(result => result)
+      .catch(error => false)
   },
+
   updateShare: async (parameters) => {
     return webApi.sendRequest({
       moduleName: 'SharedFiles',
       methodName: 'UpdateShare',
       parameters: parameters,
     })
-      .then((result) => {
-        if (result) {
-          return result
-        }
-        return false
-      })
-      .catch(() => {
-        return false
-      })
+      .then(result => result)
+      .catch(error => false)
   },
+
   getHistory: async (parameters) => {
     return webApi.sendRequest({
       moduleName: 'ActivityHistory',
       methodName: 'GetList',
       parameters: parameters,
     })
-      .then((result) => {
-        if (result) {
-          return result
-        }
-        return false
-      })
-      .catch(() => {
-        return false
-      })
+      .then(result => result)
+      .catch(error => false)
   },
+
   clearHistory: async (parameters) => {
     return webApi.sendRequest({
       moduleName: 'ActivityHistory',
       methodName: 'Delete',
       parameters: parameters,
     })
-      .then((result) => {
-        if (result) {
-          return result
-        }
-        return false
-      })
-      .catch(() => {
-        return false
-      })
+      .then(result => result)
+      .catch(error => false)
   },
+
   downloadFile: async (file) => {
     const parameters = {
       downloadUrl: file.downloadUrl,
@@ -188,59 +137,50 @@ export default {
       file,
     }
     return webApi.downloadByUrl(parameters)
-      .then((result) => {
-        if (result) return result
-        return false
-      })
-      .catch((err) => {
-        console.log(err)
-      })
+      .then(result => result)
+      .catch(error => false)
   },
-    leaveShare: async (parameters) => {
+
+  leaveShare: async (parameters) => {
+    return webApi.sendRequest({
+        moduleName: 'Files',
+        methodName: 'LeaveShare',
+        parameters: parameters,
+    })
+      .then(result => result)
+      .catch(error => false)
+  },
+
+  getContactSuggestions: async (parameters) => {
+    return webApi.sendRequest({
+        moduleName: 'Contacts',
+        methodName: 'GetContactSuggestions',
+        parameters: parameters,
+    })
+      .then(result => {
+        if (result && result.List) return result.List
+        return []
+      })
+      .catch(error => [])
+  },
+
+  updateExtendedProps: async (parameters) => {
       return webApi.sendRequest({
           moduleName: 'Files',
-          methodName: 'LeaveShare',
+          methodName: 'UpdateExtendedProps',
           parameters: parameters,
-      }).then((result) => {
-          if (result) return result
-          return false
       })
-    },
-    getContactSuggestions: async (parameters) => {
-      return webApi.sendRequest({
-          moduleName: 'Contacts',
-          methodName: 'GetContactSuggestions',
-          parameters: parameters,
-      }).then( result => {
-          if (result && result.List) return result.List
-          return []
-      }).catch( () => {
-          return []
-      })
-    },
-    updateExtendedProps: async (parameters) => {
-        return webApi.sendRequest({
-            moduleName: 'Files',
-            methodName: 'UpdateExtendedProps',
-            parameters: parameters,
-        }).then((result) => {
-            if (result) {
-                return result
-            }
-            return false
-        })
-    },
-    getExtendedProps: async (parameters) => {
-        return webApi.sendRequest({
-            moduleName: 'Files',
-            methodName: 'GetExtendedProps',
-            parameters: parameters,
-        }).then((result) => {
-            console.log(result, 'result')
-            if (result) {
-                return result
-            }
-            return false
-        })
-    },
+        .then(result => result)
+        .catch(error => false)
+  },
+
+  getExtendedProps: async (parameters) => {
+    return webApi.sendRequest({
+      moduleName: 'Files',
+      methodName: 'GetExtendedProps',
+      parameters: parameters,
+    })
+      .then(result => result)
+      .catch(error => false)
+  },
 }
